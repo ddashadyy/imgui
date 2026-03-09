f296cef7f5b7276da36c5052ef7300ea1bd27e57
ddashdyy <poletaevgena75@gmail.com>
2026-03-09 19:53:50 +0800

    premake integration

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
-    
+
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
@@ -11,18 +11,26 @@ project "ImGui"
         "imgui.h",
         "imgui.cpp",
         "imgui_draw.cpp",
-        "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
+        "imgui_demo.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_vulkan.h",
+        "backends/imgui_impl_vulkan.cpp"
     }
-    
+
+    includedirs
+    {
+        ".",
+        "backends",
+        "%{wks.location}/Flux/vendor/GLFW/include",  
+        "%{IncludeDir.Vulkan}"
+    }
+
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
         staticruntime "On"
-        
+
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
